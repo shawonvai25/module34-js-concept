9b0dfacac6389abd971c82d35554f173185f9194
shawonvai25 <abdullahalshawon25@gmail.com>
2024-12-29 02:08:56 +0600

    add

--- asyncawait.js
+++ asyncawait.js
@@ -1,11 +1,34 @@
 const myLoader = () => {
     return new Promise( (resolve, reject) => {
-        const success = true;
-        if(success){
-            resolve(0.3);
+        const success = Math.random();
+        if(success <= 0.5){
+            resolve(success);
         }
         else{
-            reject(0.7);
+            reject(success);
         }
     } )
 }
+
+myLoader()
+.then(data => console.log('resolved data',data))
+.catch( err => console.log('rejected with value',err))
+
+
+fetch('')
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.err(err))
+
+
+async function loadData(){
+    const res  = await fetch('');
+    const data = await res.json();
+    console.log(data);
+}
+
+const taskLoader = async () =>{
+   const res = await fetch('');
+}
+
+loadData();
