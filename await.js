ac256486b413a2782a773b32ddb2b4c323b22d1f
shawonvai25 <abdullahalshawon25@gmail.com>
2025-01-03 11:23:10 +0600

    add

--- await.js
+++
@@ -1,6 +0,0 @@
-function loadUser(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-}
--- context.js
+++ context.js
@@ -1,8 +1,8 @@
 let a = 5;
 function add(num1, num2){
-     const result = num1 + num2;
-     return result;
+   const result =  num1 + num2 + a;
+   return result;
 }
 
-const sum = add(4,5)
-console.log(sum);
+ const sum =  add(4,5);
+ console.log(sum);
