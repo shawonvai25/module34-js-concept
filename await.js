978c24350ece12bf514329f57d5e115d4ce28627
shawonvai25 <abdullahalshawon25@gmail.com>
2025-01-05 20:37:05 +0600

    add

--- await.js
+++ await.js
@@ -4,3 +4,26 @@ function loadUser(){
     .then(data => console.log(data))
     .catch(err => console.log(err))
 }
+
+async function loadUserAsync(){
+     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+     const data = await res.json();
+     console.log(data)
+}
+
+const loadUserArrow = async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+    console.log(data);
+}
+
+const loadUserCatch = async() =>{
+   try{
+       const res = await fetch('https://jsonplaceholder.typicode.com/users');
+       const data = await res.json();
+       console.log(data);
+   }
+   catch(error){
+
+   }
+}
